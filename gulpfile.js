5096aaf3798cfa7d8f477a426ded835923d429f3
jzh <1576803896@qq.com>
2018-12-28 20:32:55 +0800

    页面完成

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var server = require('gulp-webserver');
+
+var uglify = require('gulp-uglify');
+
+var concat = require('gulp-concat');
+
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 8686,
+        }))
+})
+
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('sass'))
+})
+
+gulp.task('dev', gulp.series('sass', 'server', 'watch'))
